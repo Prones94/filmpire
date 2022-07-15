6d307af097986259520d5ff0ee5c89bd503f0ea6
Ian <ronesp.85@gmail.com>
2022-07-14 22:23:46 -0700

    [add] alan ai added for voice functionality and added some suggestions into the script in the api

---
+++ src/components/Alan.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useContext } from 'react';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import alanBtn from '@alan-ai/alan-sdk-web';
+import { ColorModeContext } from '../utils/ToggleColorMode';
+import { fetchToken, createSessionId, moviesApi } from '../utils';
+import { selectGenreOrCategory, searchMovie } from '../features/currentGenreOrCategory';
+
+const useAlan = () => {
+  const { setMode } = useContext(ColorModeContext);
+  const dispatch = useDispatch();
+  const history = useHistory();
+  useEffect(() => {
+    alanBtn({
+      key: '113ba0fdf2be4abb905f62b9d48d6dde2e956eca572e1d8b807a3e2338fdd0dc/stage',
+      onCommand: ({ command, mode, genres, genre: genreOrCategory, query }) => {
+        if (command === 'chooseGenre') {
+          const foundGenre = genres.find((g) => g.name.toLowerCase() === genreOrCategory.toLowerCase());
+          if (foundGenre) {
+            history.push('/');
+            dispatch(selectGenreOrCategory(foundGenre.id));
+          } else {
+            const category = genreOrCategory.startsWith('top') ? 'top_rated' : genreOrCategory;
+            history.push('/');
+            dispatch(selectGenreOrCategory(category));
+          }
+        } else if (command === 'changeMode') {
+          if (mode === 'light') {
+            setMode('light');
+          } else {
+            setMode('dark');
+          }
+        } else if (command === 'login') {
+          fetchToken();
+        } else if (command === 'logout') {
+          localStorage.clear();
+          history.push('/');
+        } else if (command === 'search') {
+          dispatch(searchMovie(query));
+        }
+      },
+    });
+  }, []);
+  return (
+    <div>Alan</div>
+  );
+};
+
+export default useAlan;
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import { CssBaseline } from '@mui/material';
 import { Route, Switch } from 'react-router-dom';
 
 // eslint-disable-next-line import/no-named-as-default
 import useStyles from './styles';
+import useAlan from './Alan';
 
 import { NavBar, Movies, Profile, MovieInformation, Actors } from './index';
 
 function App() {
   const classes = useStyles();
+  const alanBtnContainer = useRef()
+  useAlan();
   return (
     <div className={classes.root}>
       <CssBaseline />
@@ -30,6 +33,7 @@ function App() {
           </Route>
         </Switch>
       </main>
+      <div ref={alanBtnContainer} />
     </div>
   );
 }
